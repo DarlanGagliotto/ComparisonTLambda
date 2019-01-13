2e53fafb067a10d47745877136311e4a3e533608
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-01-13 17:16:11 -0200

    Comparison T com lambda

--- ComparisonTLambda/Program.cs
+++ ComparisonTLambda/Program.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComparisonTLambda
+namespace ComparisonT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            List<Product> item = new List<Product>();
+
+            item.Add(new Product("TV", 900.00));
+            item.Add(new Product("Geladeira", 1200.00));
+            item.Add(new Product("Tablet", 1630.00));
+
+            // public delegate int Comparison<in T>(T x, T y);
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToString().ToUpper().CompareTo(p2.Name.ToString().ToUpper());
+
+            item.Sort(comp);
+
+            foreach (Product prod in item)
+            {
+                Console.WriteLine(prod);
+            }
         }
     }
 }
